37c826161e637b2b9201567f17770d7184d316f2
Hamnah <hamnahq25@gmail.com>
2023-03-21 02:30:16 -0400

    Deleted the solutiona and current guess text

--- front-end/src/components/Wordle.js
+++ front-end/src/components/Wordle.js
@@ -28,8 +28,6 @@ export default function Wordle({solution}) {
 
     return (
         <div>
-            <div> solution - {solution} </div>
-            <div>current guess - {currentGuess}</div>
             <Grid currentGuess={currentGuess} guesses={guesses} turn ={turn}/>
             <Keypad usedKeys={usedKeys}/>
             {showModal && <Modal isCorrect = {isCorrect} turn ={turn} solution = {solution} />}
